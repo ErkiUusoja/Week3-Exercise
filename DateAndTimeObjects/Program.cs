d0a021c913f30b49762784de5a72ebb66421fb3b
Opilane TTHK <opilane@tthk.ee>
2021-09-15 12:14:39 +0300

    master

---
+++ DateAndTimeObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DateAndTimeObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime current = DateTime.Now;
+            /* Console.WriteLine(current.Hour);
+             Console.WriteLine(current.Minute);
+             Console.WriteLine(current.Second);
+             Console.WriteLine(current.Millisecond);*/
+            Console.WriteLine(current.ToString("dddd dd MMMM"));
+            Console.WriteLine(current.ToString());
+            
+                
+        }
+    }
+}
---
+++ Week3Exercises/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Week3Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tingimused sisseastumiseks:
+            //miinimum matemaatikas 85 punkti;
+            //miinimum keemias 90 punkti;
+            //miinimum bioloogias 95 punkti;
+            //programm küsib kasutajal sisestada tema
+            //eksami punktid ja otsustab, kas kasutaja
+            //saab arstiks õppida
+
+            Console.WriteLine("Siseta oma matemaatika eksami punktid");
+            int math = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Siseta oma keemia eksami punktid");
+            int chemistry = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Siseta oma bioloogia eksami punktid");
+            int biology = Convert.ToInt32(Console.ReadLine());
+
+            if(math >= 85 && chemistry >= 90 && biology >= 95)
+            {
+                Console.WriteLine("Tubli! saad arstiks õppida.");
+            }
+            else
+            {
+                Console.WriteLine("Proovi järgmine aasta.");
+            }
+
+
+
+
+        }
+    }
+}
